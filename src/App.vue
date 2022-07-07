4068e46f44fda8545747ec0135d2657b6b3b19f0
pietro <pietro.pizzuto@gmail.com>
2022-07-07 20:12:29 +0200

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="fixed-top bg-warning" >
+        <div class="container p-0">
+<b-navbar toggleable="lg" type="dark" variant="warning" >
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item><router-link to="/"> Home</router-link></b-nav-item>
+        <b-nav-item><router-link to="/about"> About</router-link></b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+        </div>
+
+    </div>
+    
+    <div id="app-main" class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+@import './assets/site.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#app-main {
+  margin-top: 5rem;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <form id="form-login" action="/login" method="post" novalidate="novalidate">
+	<div class="form-group">
+		<label>Email</label><input type="email" name="email" class="form-control" placeholder="Enter email" value="">
+	</div>
+	<div class="form-group">
+		<label>Password</label> <input type="password" name="password" class="form-control" placeholder="Password" value="">
+		<div class="my-3 text-right">
+			<a href="/password" class="text-muted" data-pjax-state="">Forgot password?</a>
+		</div>
+	</div>
+	<div class="checkbox mb-3">
+		<label class="ui-check"><input type="checkbox" name="remember" value="true"><i></i> Remember me</label>
+	</div>
+	<input type="hidden" name="exec" value="true">
+	<button type="submit" class="btn btn-primary btn-block">Sign In</button>
+	<div>
+		Do not have an account? <a href="/register" class="text-primary">Sign up</a>
+	</div>
+</form>
+  </div>
+</template>
